69b8d8308d82e8a58821ed00b1840105176079eb
Konstantin Karuna <johnysalvesen@gmail.com>
2021-12-02 12:16:18 +0300

    Removed unnecessary files;

---
+++ src/components/common/constants.js
@@ -0,0 +1,11 @@
+const size = {
+  xs: "576px",
+  md: "768px",
+  lg: "991px",
+};
+
+export const constants = {
+  xs: `(min-width: ${size.xs})`,
+  md: `(min-width: ${size.md})`,
+  lg: `(min-width: ${size.lg})`,
+};
